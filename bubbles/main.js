ed31c94316392ea4f19ba33c2da5b8615b1bfea2
Kyle Chezik <kylechezik@d207-023-161-096.wireless.sfu.ca>
2016-05-06 16:07:23 -0700

    Initial Submit: Personal Website Files
    Creating a repo. for my personal website.

---
+++ bubbles/main.js
@@ -0,0 +1,15 @@
+var amber = [43, 74, 49];
+var mahogany = [20, 100, 38];
+var green = [158, 100, 33];
+var blue = [207, 42, 63];
+var purple = [329, 31, 50];
+
+var myName = "Dat Stat Vis -- Ecology | Kyle Chezik";
+var letterColors = [mahogany, amber, green, purple, blue];
+
+// bubbleShape = "circle";
+bubbleShape = "square";
+
+
+drawName(myName, letterColors);
+bounceBubbles()
