165e2f66f8be37a4203b32fcee4270cbdcf0b159
Kyle Chezik <kylechezik@D-10-156-3-231.dhcp4.washington.edu>
2016-05-15 14:27:23 -0700

    Reformat html layout.

--- bubbles/main.js
+++ bubbles/main.js
@@ -6,10 +6,12 @@ var purple = [329, 31, 50];
 var cantaloupe = [41,95,70];
 var honeydew = [20,93,68];
 var strawberry = [344,93,65]
+var black = [0,0,20]
 
 var myName = "Tangible Data|Kyle Chezik PhC"
 // var letterColors = [mahogany, amber, green, purple, blue];
 var letterColors = [cantaloupe, amber, honeydew, strawberry];
+// var letterColors = [black];
 
 // bubbleShape = "circle";
 bubbleShape = "square";
