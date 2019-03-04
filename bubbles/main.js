f85f2747e3ae740e6edf3dd9bc06cfa38cceb410
Kyle Chezik <kchezik@gmail.com>
2019-03-04 10:12:26 -0800

    Updated title to reflect transition to data science

--- bubbles/main.js
+++ bubbles/main.js
@@ -1,20 +1,20 @@
-var amber = [43, 74, 49];
-var mahogany = [20, 100, 38];
-var green = [158, 100, 33];
-var blue = [207, 42, 63];
-var purple = [329, 31, 50];
+var amber = [43,74,49];
+var mahogany = [20,100,38];
+var green = [158,100,33];
+var blue = [207,42,63];
+var purple = [329,31,50];
 var cantaloupe = [41,95,70];
 var honeydew = [20,93,68];
 var strawberry = [344,93,65]
 var black = [0,0,20]
 
-var myName = "Tangible Data|Kyle Chezik PhC"
+var myName = "Kyle Chezik PhD"
 // var letterColors = [mahogany, amber, green, purple, blue];
 var letterColors = [cantaloupe, amber, honeydew, strawberry];
 // var letterColors = [black];
 
-// bubbleShape = "circle";
-bubbleShape = "square";
+bubbleShape = "circle";
+// bubbleShape = "square";
 
 
 drawName(myName, letterColors);
