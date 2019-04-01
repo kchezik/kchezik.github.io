01e39a302dde5bff00a6a65ed45bf32306459002
Kyle Chezik <kchezik@gmail.com>
2019-03-31 19:31:52 -0700

    added PhD projects

--- bubbles/main.js
+++ bubbles/main.js
@@ -7,10 +7,15 @@ var cantaloupe = [41,95,70];
 var honeydew = [20,93,68];
 var strawberry = [344,93,65]
 var black = [0,0,20]
+var orange = [40,100,64]
+var bluish = [225,55,55]
+var greenish = [162,70,45]
+var yellowish= [50,80,62]
 
 var myName = "Kyle Chezik PhD"
 // var letterColors = [mahogany, amber, green, purple, blue];
-var letterColors = [cantaloupe, amber, honeydew, strawberry];
+//var letterColors = [cantaloupe, amber, honeydew, strawberry];
+var letterColors = [orange, bluish, greenish, yellowish];
 // var letterColors = [black];
 
 bubbleShape = "circle";
